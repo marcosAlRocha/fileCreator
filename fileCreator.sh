9a0d7e51a911d443d7d0b89ff5fc8a7872e2187c
Marcos Al. Rocha <marcosar@gmail.com>
2020-02-06 10:23:25 -0200

    Correção na geração dos arquivos das classes de implementação de interfaces

--- fileCreator.sh
+++ fileCreator.sh
@@ -16,7 +16,7 @@ do
 	fi
   printf "package "$basePackageName".domain;\n\nimport java.io.Serializable;\nimport javax.persistence.*;\n\n@Entity\npublic class "$class" implements Serializable {\n @Id\n private Long id$class;\n}" > domain/"$class".java
   printf "package "$basePackageName".service;\n\npublic interface "$class"Service {\n\n}" > service/"$class"Service.java
-  printf "package "$basePackageName".service.serviceImpl;\n\nimport org.springframework.stereotype.Service;import "$basePackageName".service."$class"Service;\n\n\n\n@Service\npublic class "$class"ServiceImpl implements "$class"Service {\n\n}" > service/ServiceImpl/"$class"ServiceImpl.java
+  printf "package "$basePackageName".service.serviceImpl;\n\nimport org.springframework.stereotype.Service;import "$basePackageName".service."$class"Service;\n\n\n\n@Service\npublic class "$class"ServiceImpl implements "$class"Service {\n\n}" > service/serviceImpl/"$class"ServiceImpl.java
   printf "package "$basePackageName".repository;\n\nimport org.springframework.data.jpa.repository.JpaRepository;\nimport org.springframework.stereotype.Repository;\nimport "$basePackageName".domain."$class";\n\n@Repository\npublic interface "$class"Repository extends JpaRepository <"$class", Long> {\n\n}" > repository/"$class"Repository.java
   printf "package "$basePackageName".resource;\n\nimport org.springframework.web.bind.annotation.RestController;\n\n@RestController\npublic class "$class"Resource {\n\n}" > resource/"$class"Resource.java
 done
