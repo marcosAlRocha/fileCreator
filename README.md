e3e9ce7425094437c96ddf6fb7ca0f7b278543c6
Marcos Al. Rocha <marcosar@gmail.com>
2020-02-16 09:43:21 -0300

    Adição de melhorias, verificação se diretório já existia, inclusão das classes de teste, alteração na descrição

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # fileCreator
 
-Script utilitário desenvolvido para auxiliar na criação dos arquivos de um projeto Java
+Script utilitário desenvolvido para auxiliar na criação de arquivos em projeto Java com SpringBoot, JPA, JUnit, H2
+
+O problema é que durante o mapeamento de uma entidade, muitas vezes precisamos replicar os arquivos para as todas as pastas com seu respectivo papel. 
+Este script visa agilizar este processo.
 
 Para seu funcionamento, é necessário que haja uma convenção para a estrutra de pastas do projeto, no meu caso ficou do seguinte modo : 
 
@@ -10,8 +13,13 @@ Para seu funcionamento, é necessário que haja uma convenção para a estrutra 
   - repository para as classes de persistência
   - resource para os controladores rest
 
-O problema é que durante o mapeamento de uma entidade, muitas vezes precisamos replicar os arquivos para as todas as pastas com seu respectivo papel, e este script visa agilizar este processo.
+  = Mesma estrutura se replica para arquivos de testes
 
-Basta copíá-lo para a pasta raiz do projeto, onde se localiza a classe main,  e executar via terminal. A medida que são criados os arquivos das classes domain, todos os outros arquivos são criados e já pré configurados com um setup mínimo de funcionamento. 
+Basta copíá-lo para a pasta raiz do projeto, onde se localiza o pom.xml, e executar via terminal. A medida que são criados os arquivos das classes domain, todos os outros arquivos são criados e já pré configurados com um setup mínimo de funcionamento. 
+
+Caso ao subir a aplicação ocorra erro de mapeamento de Url - Failed to configure a DataSource: 'url' attribute is not specified and no embedded datasource could be configured - basta colocar <scope>provided</scope> na dependência do starter data-jpa.
+
+
+
 
 
--- fileCreator.sh
+++ fileCreator.sh
@@ -1,11 +1,53 @@
-echo "Digite o nome do pacote base de sua aplicação"
+echo "Informe o grupo de sua aplicação com o nome do artefato: (Exemplo: com.minhaempresa.minhaapi)"
 read basePackageName
 
-mkdir domain
-mkdir service
-mkdir service/serviceImpl
-mkdir repository
-mkdir resource
+echo "Digite o nome da classe principal de sua aplicação, sem a extensão:"
+read mainClassName
+
+pathToMainClass=$(find . -name $mainClassName.java -printf '%h\n')
+pathToMainClassTest=$(find . -name "$mainClassName"Tests.java -printf '%h\n')
+
+if [ ! -d $pathToMainClass"/domain" ] 
+then
+	mkdir $pathToMainClass"/domain"
+fi
+
+if [ ! -d $pathToMainClass"/service" ] 
+then
+	mkdir $pathToMainClass"/service"
+fi
+
+if [ ! -d $pathToMainClass"/service/serviceImpl" ] 
+then
+	mkdir $pathToMainClass"/service/serviceImpl"
+fi
+
+if [ ! -d $pathToMainClass"/repository" ]
+then
+	mkdir $pathToMainClass"/repository"
+fi	
+
+
+if [ ! -d $pathToMainClass"/resource" ]
+then
+	mkdir $pathToMainClass"/resource"
+fi
+
+if [ ! -d $pathToMainClassTest"/service" ] 
+then
+	mkdir $pathToMainClassTest"/service"
+fi
+
+if [ ! -d $pathToMainClassTest"/repository" ]
+then	
+	mkdir $pathToMainClassTest"/repository"
+fi	
+
+if [ ! -d $pathToMainClassTest"/resource" ]
+then	
+	mkdir $pathToMainClassTest"/resource"
+fi
+
 
 while :
 do
@@ -14,12 +56,104 @@ do
 	then
 		break
 	fi
-  printf "package "$basePackageName".domain;\n\nimport java.io.Serializable;\nimport javax.persistence.*;\n\n@Entity\npublic class "$class" implements Serializable {\n @Id\n private Long id$class;\n}" > domain/"$class".java
-  printf "package "$basePackageName".service;\n\npublic interface "$class"Service {\n\n}" > service/"$class"Service.java
-  printf "package "$basePackageName".service.serviceImpl;\n\nimport org.springframework.stereotype.Service;import "$basePackageName".service."$class"Service;\n\n\n\n@Service\npublic class "$class"ServiceImpl implements "$class"Service {\n\n}" > service/serviceImpl/"$class"ServiceImpl.java
-  printf "package "$basePackageName".repository;\n\nimport org.springframework.data.jpa.repository.JpaRepository;\nimport org.springframework.stereotype.Repository;\nimport "$basePackageName".domain."$class";\n\n@Repository\npublic interface "$class"Repository extends JpaRepository <"$class", Long> {\n\n}" > repository/"$class"Repository.java
-  printf "package "$basePackageName".resource;\n\nimport org.springframework.web.bind.annotation.RestController;\n\n@RestController\npublic class "$class"Resource {\n\n}" > resource/"$class"Resource.java
+ 
+printf "package "$basePackageName".domain;
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+public class "$class" implements Serializable {
+
+	@Id private Long id$class;
+
+}" > "$pathToMainClass"/domain/"$class".java
+
+  
+printf "package "$basePackageName".service;
+
+public interface "$class"Service { 
+
+}" > "$pathToMainClass"/service/"$class"Service.java
+
+
+printf "package "$basePackageName".service.serviceImpl;
+
+import org.springframework.stereotype.Service;
+import "$basePackageName".service."$class"Service;
+
+@Service
+public class "$class"ServiceImpl implements "$class"Service {
+
+}" > "$pathToMainClass"/service/serviceImpl/"$class"ServiceImpl.java
+  
+printf "package "$basePackageName".repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import "$basePackageName".domain."$class";
+
+@Repository
+public interface "$class"Repository extends JpaRepository <"$class", Long> { 
+
+}" > "$pathToMainClass"/repository/"$class"Repository.java
+
+
+printf "package "$basePackageName".resource;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class "$class"Resource {
+
+}" > "$pathToMainClass"/resource/"$class"Resource.java
+
+
+
+printf "package "$basePackageName".resource;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("\""test"\"")
+//@WebMvcTest(controllers = nome da sua classe controller.class)
+@AutoConfigureMockMvc
+public class "$class"ResourceTest {
+
+}" > "$pathToMainClassTest"/resource/"$class"ResourceTest.java
+
+
+printf "package "$basePackageName".repository;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("\""test"\"")
+@DataJpaTest
+public class "$class"RepositoryTest {
+
+}" > "$pathToMainClassTest"/repository/"$class"RepositoryTest.java
+
+
+printf "package "$basePackageName".service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("\""test"\"")
+public class "$class"ServiceTest {
+
+}" > "$pathToMainClassTest"/service/"$class"ServiceTest.java
+
+
+
+
 done
-
-
-
